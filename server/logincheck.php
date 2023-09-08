7fa090a64ab3c322682e16f8de329a6f54308474
omg1410-gadre <136463074+omg1410-gadre@users.noreply.github.com>
2023-09-08 22:32:12 +0530

    Update logincheck.php

--- server/logincheck.php
+++ server/logincheck.php
@@ -4,6 +4,7 @@ include 'classes/usercheck.php';
 
 $usernameInfo=$_POST['username'];
 $passwordInfo=$_POST['password'];
+$passwordInfo=md5($passwordinfo);
 
 $user=new User;
 $user->insertUser($usernameInfo,$passwordInfo);
